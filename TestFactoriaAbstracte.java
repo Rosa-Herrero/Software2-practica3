d700b4d5115757e884fe8cbb39e6f6b035640aca
Rosa-Herrero <63284656+Rosa-Herrero@users.noreply.github.com>
2020-05-18 08:52:41 +0200

    update template

---
+++ TestFactoriaAbstracte.java
@@ -0,0 +1,44 @@
+
+package CapaPresentacio;
+
+import CapaAplicacio.Registre;
+
+import java.util.HashSet;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestFactoriaAbstracte {
+	
+	static HashSet <Float>  resultatEsperat;
+
+	@BeforeClass
+	public static void TestVenda() {
+		resultatEsperat = new HashSet <Float> ();
+        //Suposem que el metode getTotalAbansDte() de la classe Venda retorna 80.5
+		resultatEsperat.add(80.5F);//resultat esperat pel descompte null
+        
+        resultatEsperat.add(78.00F);//resultat esperat pel descompte absolut
+        
+        resultatEsperat.add(76.475F);//resultat esperat pel descompte del percentatge 0.05
+        resultatEsperat.add(79.695F);//resultat esperat pel descompte del percentatge 0.01
+        resultatEsperat.add(72.45F);//resultat esperat pel descompte del percentatge 0.10
+        
+	}	
+	
+	@Test
+	public void testGetTotalVenda() {
+		try {
+			Registre ctl = new Registre ();
+	        float totalVenda = ctl.getTotalVenda ();
+	        System.out.println ("Import Total de la venda: " + totalVenda);
+	        System.out.println(ctl.getDescompte());
+	        assertTrue ("El resultat obtingut es: " + totalVenda, resultatEsperat.contains(totalVenda));
+		} catch (Exception ex) {
+			System.out.println ("ERROR Registre.getTotalVenda(): " + ex.getMessage());
+		}		
+	}
+	
+
+
+}
